b2d5490147b84f7581a3aa376917538b06bce446
Dmytro Kasko <dimoks77@gmail.com>
2024-05-20 16:36:12 +0200

    color

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,16 +4,17 @@ import css from './Loader.module.css';
 
 export const Loader: React.FC = () => {
   return (
-    <ProgressBar
-      visible={true}
-      height="200"
-      width="100"
-      color="#3747ac"
-      borderColor="#3747ac"
-      ariaLabel="progress-bar-loading"
-      wrapperStyle={{}}
-      wrapperClass={css.loader}
-    />
+    <div className={css.loader}>
+      <div className={css.progressBar}>
+        <ProgressBar
+          visible={true}
+          height={200}
+          width={100}
+          borderColor="#3747ac"
+          ariaLabel="progress-bar-loading"
+        />
+      </div>
+    </div>
   );
 };
 
